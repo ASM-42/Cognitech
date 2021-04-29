862d0c4cfa363abf4247aa2d9303b8b8a8df7e8f
martinpochat <histoiredemartin@gmail.com>
2021-04-29 20:55:11 +0200

    Editer son profil

--- Cognitech/profil.php
+++ Cognitech/profil.php
@@ -30,13 +30,23 @@ $result = $sql -> fetch();
 <div class="pageProfil">
     <div class = "profil">
         <div class="headerProfil"><img src="images/imagePageProfil/icons8-utilisateur-96.png" id="imagecontact"> <?php echo $result['prenom'] . ' ' .  '<b>' . $result['nom'];?></div>
-        <button class="boutonProfil"><?php echo htmlentities(trim($_SESSION['email'])); ?></button>
+
+        <form action="profile_update.php" method="post">
+        <input type="email" class="boutonProfil" name="email" value="<?php echo htmlentities(trim($_SESSION['email'])); ?>">
+<!--        <button class="boutonProfil">--><?php //echo htmlentities(trim($_SESSION['email'])); ?><!--</button>-->
         <button class="boutonMDP">password</button>
-        <button class="boutonSexe"><?php echo $result['sexe']; ?></button>
-        <button class="boutonTelephone">0<?php echo $result['phone']; ?></button>
-        <button class="boutonEcurie"><?php echo $result['ecurie']; ?></button>
-        <button class="boutonDocteur">Dr.<?php echo $result['medecin']; ?></button>
-        <button class="boutonAnniversaire"><?php echo $result['dateDeNaissance']; ?></button>
+        <input type="text" class="boutonSexe" name="sexe" value="<?php echo $result['sexe']; ?>">
+<!--        <button class="boutonSexe">--><?php //echo $result['sexe']; ?><!--</button>-->
+        <input type="text" class="boutonTelephone" name="phone" value="0<?php echo $result['phone']; ?>">
+<!--        <button class="boutonTelephone">0--><?php //echo $result['phone']; ?><!--</button>-->
+        <input type="text" class="boutonEcurie" name="ecurie" value="<?php echo $result['ecurie']; ?>">
+<!--        <button class="boutonEcurie">--><?php //echo $result['ecurie']; ?><!--</button>-->
+        <input type="text" class="boutonDocteur" name="medecin" value="<?php echo $result['medecin']; ?>">
+<!--        <button class="boutonDocteur">Dr.--><?php //echo $result['medecin']; ?><!--</button>-->
+        <input type="text" class="boutonAnniversaire" name="dateDeNaissance" value="<?php echo $result['dateDeNaissance']; ?>">
+<!--        <button class="boutonAnniversaire">--><?php //echo $result['dateDeNaissance']; ?><!--</button>-->
+        <input type="submit" name="edit">
+        </form>
     </div>
 </div>
 </body>
