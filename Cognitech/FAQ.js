2929ebd5a75d5bb4470acb03c4c071d00dda7333
LouisStefani <78175392+LouisStefani@users.noreply.github.com>
2021-05-10 18:48:14 +0200

    FAQ

--- Cognitech/FAQ.js
+++ Cognitech/FAQ.js
@@ -1,19 +1,62 @@
 
 
-
-var btnPopup = document.getElementById('btnPopup');
-var overlay = document.getElementById('overlay');
-concole.log(overlay.style.display);
-btnPopup.onclick = function() {
-    overlay.style.display = "block";
+/* ouverture popup réponse 1*/
+var overlay1 = document.getElementById('overlay1');
+function on1() {
+    overlay1.style.display = "block";
 }
 
-var btnClose = document.getElementById('btnClose');
-btnClose.onclick = function() {
-    overlay.style.display = "none";
+/* ouverture popup réponse 2*/
+var overlay2 = document.getElementById('overlay2');
+function on2() {
+    overlay2.style.display = "block";
 }
-window.onclick = function(event) {
-    if (event.target == overlay) {
-        overlay.style.display = "none";
-    }
+
+/* ouverture popup réponse 3*/
+var overlay3 = document.getElementById('overlay3');
+function on3() {
+    overlay3.style.display = "block";
+}
+
+/* ouverture popup réponse 4*/
+var overlay4 = document.getElementById('overlay4');
+function on4() {
+    overlay4.style.display = "block";
+}
+
+/* ouverture popup réponse 5*/
+var overlay5 = document.getElementById('overlay5');
+function on5() {
+    overlay5.style.display = "block";
+}
+
+
+/* fermeture popup réponse1*/
+function off1() {
+    overlay1.style.display = "none";
+}
+
+
+/* fermeture popup réponse2*/
+function off2() {
+    overlay2.style.display = "none";
+}
+
+
+/* fermeture popup réponse3*/
+function off3() {
+    overlay3.style.display = "none";
+}
+
+
+
+/* fermeture popup réponse4*/
+function off4() {
+    overlay4.style.display = "none";
+}
+
+
+/* fermeture popup réponse5*/
+function off5() {
+    overlay5.style.display = "none";
 }
