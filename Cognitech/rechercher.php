8844fd7823059c101f0146b642a6832d0f02d906
Band61586 <80159832+Band61586@users.noreply.github.com>
2021-05-24 15:16:16 +0200

    statistique gestio page

--- Cognitech/rechercher.php
+++ Cognitech/rechercher.php
@@ -70,7 +70,7 @@ $sql = $connexion->query(
         echo "<ul>\n";
         while( $r = mysqli_fetch_array($sql)) {
             $user_id = $r['id'];
-            echo "<a href='profil_search.php?id=$user_id'>";
+            echo "<a href='StatistiqueGestio.php?id=$user_id'>";
             echo "<li>";
             echo "<div class=\"cont\">";
             echo "<div class=\"cont1\">";
