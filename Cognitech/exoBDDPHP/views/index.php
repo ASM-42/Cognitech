aa744a5d6b65ff80f078cf6b287993bd91a60418
Martin Pochat-Cottilloux <72661166+martinpctx@users.noreply.github.com>
2021-06-01 18:53:50 +0200

    Delete Cognitech/exoBDDPHP directory

--- Cognitech/exoBDDPHP/views/index.php
+++
@@ -1,9 +0,0 @@
-<html>
-<head>
-    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-    <title>Page principale du site</title>
-</head>
-<body>
-<a href="accueil.php">Espace membre</a>
-</body>
-</html>
